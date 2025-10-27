5ba03e7c3ae14d610ee9a1cf84dec5d3aa3bb950
SeoHyeon <tjgus1668@naver.com>
2025-10-27 14:46:46 +0900

    fix: add tool index.ts

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,7 +1,9 @@
 import { searchProblemTool } from "@/tools/search-problem";
 import { tagListTool } from "@/tools/tag-list";
+import { searchTagTool } from "./search-tag";
 
 export const allTools = [
   searchProblemTool,
   tagListTool,
+  searchTagTool,
 ];
